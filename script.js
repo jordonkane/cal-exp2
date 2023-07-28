efd19e49718c206bd2eabead3a843ebe48e34aed
Jordon Kane <kanejord99@outlook.com>
2023-07-28 16:26:24 -0400

    Added height selection input

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function calculate() {
 
     let age = document.getElementById('age').value
     let sex = document.querySelector('input[name="sex"]:checked').value;
-    let height = document.getElementById('height').value
     let weight = document.getElementById('weight').value
+
+    selectHeightElement = document.querySelector('#height');
+    height = selectHeightElement.options[selectHeightElement.selectedIndex].value;
 
     let calories = (sex == "male") ?
     ((10 * weight / 2.2) + (6.25 * height / 0.393701) - (5 * age) + 5) :
@@ -22,14 +24,13 @@ function calculate() {
     selectActivityElement = document.querySelector('#activity');
     activity = selectActivityElement.options[selectActivityElement.selectedIndex].value;
     
-    // document.getElementById('heading').innerHTML = "Your Basal Metabolic Rate is: "
-    // document.getElementById('calorie-output').innerHTML = calories.toFixed(0) + " cals/day"
-
     if (activity == "sedentary") {
 
+        newCalories = (calories * 1.2).toFixed(0)
+
         document.getElementById('heading').innerHTML = "Your caloric expenditure is: "
-        document.getElementById('calorie-output').innerHTML = (calories * 1.2).toFixed(0) + " cals/day"
-    
+        document.getElementById('calorie-output').innerHTML = newCalories + " cals/day"
+
     } else if (activity == "light") {
 
         document.getElementById('heading').innerHTML = "Your caloric expenditure is: "
@@ -51,4 +52,8 @@ function calculate() {
         document.getElementById('calorie-output').innerHTML = (calories * 1.9).toFixed(0) + " cals/day"
     
     }
+}
+
+function {
+    document.getElementById('message').innerHTML = "Eat " + (newCalories - (-500)) + " cals/day to gain weight <br> Eat " + (newCalories - 500) + "cals/day to lose weight <br>"
 }
